9a18520f8466a2571e7df3f2ce27cb2cc1834c4c
Goffin Simon <simon.goffin@outlook.com>
2014-03-17 22:04:17 +0100

    Transfert vue.jsp dans WEB-INF + actualisation de Test.java.

--- src/com/sdzee/servlets/Test.java
+++ src/com/sdzee/servlets/Test.java
@@ -9,19 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Test extends HttpServlet {
-	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-		response.setContentType("text/html"); 
-		response.setCharacterEncoding( "UTF-8" ); 
-		PrintWriter out = response.getWriter(); 
-		out.println("<!DOCTYPE html>"); 
-		out.println("<html>"); 
-		out.println("<head>");
-		out.println("<meta charset=\"utf-8\" />"); 
-		out.println("<title>Test</title>"); 
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<p>Ceci est une page générée depuis une servlet.</p>"); 
-		out.println("</body>");
-		out.println("</html>"); }
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
+	{
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/vue.jsp" ).forward( request, response );
+	}
 
 }
